6ab701518344533fe92cbf653d9fd86b147d1e77
stissshak <stishak.oleg@gmail.com>
2026-05-16 18:33:53 +0500

    Little commit, improving Logger

--- inc/Logger.hpp
+++ inc/Logger.hpp
@@ -11,37 +11,6 @@ public:
     };
     
     virtual void log(Level l, std::string_view msg) = 0;
-    void info(std::string_view msg);
-    void warning(std::string_view msg);
-    void error(std::string_view msg);
-
-    void set_debug(bool state){
-        isDebug = state;
-    }
-
-    virtual ~Logger() {}
-    
-protected:
-    bool isDebug = true;
-};
-
-class ConsoleLogger : public Logger{
-public:
-    void log(Level l, std::string_view msg) override{
-        if(isDebug == 1 && l == Level::Info){
-            std::cout << "\x1B[38;5;2m[INFO]\x1B[0m ";
-            std::cout << msg << std::endl;
-        }
-        else if(isDebug == 1 && l == Level::Warning){
-            std::cout << "\x1B[38;5;3m[WARNING]\x1B[0m ";
-            std::cout << msg << std::endl;
-        }
-        else if(l == Level::Error){
-            std::cout << "\x1B[38;5;1m[ERROR]\x1B[0m ";
-            std::cout << msg << std::endl;
-        }
-        
-    }
     
     void info(std::string_view msg){
         log(Level::Info, msg);
@@ -54,19 +23,49 @@ public:
     void error(std::string_view msg){
         log(Level::Error, msg);
     }
+
+    void set_min_level(Level l){
+        minLevel = l;
+    }
+
+    virtual ~Logger() {}
+    
+protected:
+    Level minLevel = Level::Info;
+};
+
+class ConsoleLogger : public Logger{
+public:
+    void log(Level l, std::string_view msg) override{
+        if(l < minLevel){
+            std::cerr << "\x1B[38;5;2m[INFO]\x1B[0m ";
+            std::cerr << msg << std::endl;
+        }
+        else if(l < minLevel){
+            std::cerr << "\x1B[38;5;3m[WARNING]\x1B[0m ";
+            std::cerr << msg << std::endl;
+        }
+        else if(l == Level::Error){
+            std::cerr << "\x1B[38;5;1m[ERROR]\x1B[0m ";
+            std::cerr << msg << std::endl;
+        }
+        
+    }
+    
+    
 };
 
 class FileLogger : public Logger{
 public:
-    FileLogger(std::string_view file_name){
-        file.open(file_name.data());
+    FileLogger(const std::string& file_name){
+        if(!file.is_open()) throw std::runtime_error("cannot open log file: " + file_name);
     }
     void log(Level l, std::string_view msg) override{
-        if(isDebug == 1 && l == Level::Info){
+        if(l < minLevel){
             file << "[INFO]";
             file << msg << std::endl;
         }
-        else if(isDebug == 1 && l == Level::Warning){
+        else if(l < minLevel){
             file << "[WARNING]";
             file << msg << std::endl;
         }
@@ -76,17 +75,6 @@ public:
         }
     }
     
-    void info(std::string_view msg){
-        log(Level::Info, msg);
-    }
-    
-    void warning(std::string_view msg){
-        log(Level::Warning, msg);
-    }
-    
-    void error(std::string_view msg){
-        log(Level::Error, msg);
-    }
 private:
     std::ofstream file;
 };
